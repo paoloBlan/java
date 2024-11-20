51c29aec4adcff7828698a18fba58535cb84317a
paoloBlan <0002160013@unu.edu.pe>
2024-11-20 11:11:59 -0500

    clase 11 - conexion

---
+++ mysql2/src/main/java/com/conexion/Gestor/Conexion.java
@@ -0,0 +1,33 @@
+package com.conexion.Gestor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+   private String url = "";
+   private String user = "";
+   private String pass = "";
+   private Connection conexion = null;
+   
+   public void conectar(){
+        try {
+             conexion = DriverManager.getConnection(url, user, pass);
+             if(conexion!=null){
+                System.out.println("Conexion okay");
+             }
+        } catch (Exception e) {
+            System.out.println("error al conectar: "+e.getMessage());
+        }
+    
+   }
+
+   public void desconectar(){
+        try {
+            conexion.close();
+            System.out.println("conexion cerrada");
+        } catch (Exception e) {
+           System.out.println("Error al cerrar la conexion: "+e.getMessage());
+        }
+   }
+   
+}
